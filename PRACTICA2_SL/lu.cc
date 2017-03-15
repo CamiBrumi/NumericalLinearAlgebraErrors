5e359b7d5a02682329af069f6bf65ccb71129f0c
Cami <cameliabrumar@gmail.com>
2017-03-15 14:06:31 +0100

    treball de la lu.cc amb el becari
    compila i funciona correctament!!

--- PRACTICA2_SL/lu.cc
+++ PRACTICA2_SL/lu.cc
@@ -6,7 +6,7 @@ using namespace std;
 typedef vector<double> fila;
 typedef vector<fila> matriu;
 
-void swap(fila a, fila b) { // intercanvia dues files
+void swap(fila& a, fila& b) { // intercanvia dues files
 	fila c = a;
 	a = b;
 	b = c;
@@ -16,41 +16,38 @@ void swap(fila a, fila b) { // intercanvia dues files
 // no se com fer la matriu perm
 // com mirem si hi ha un nombre parell/senar de permutacions?
 // aquest programa em compila però no em dóna el resultat esperat
-int pivotatge_parcial(matriu& a, int n, fila perm, double tol, int k, double& pivot) {
-
-	for(int i = k + 1; i <= n - 1; ++i) {
+int pivotatge_parcial(matriu a, int n, double tol, int k) {
+	for(int i = k; i <= n - 1; ++i) {
 			double max_fila = abs(a[i][k]);
 			for(int j = k + 1; j <= n - 1; ++j) { // busquem el màxim (en valor absolut) de la fila i-èssima
-				double aij = abs(a[i][j]);
-				if (max_fila < aij) max_fila = aij; 
+				max_fila = max(max_fila, abs(a[i][j]));
 			}
 			for(int j = k; j <= n - 1; ++j) { // dividim la fila pel màxim
 				a[i][j] /= max_fila; 
 			}
 	}
-	double max_col = abs(a[k + 1][k]);
-	int max_pos = k + 1;
-	for(int i = k + 2; i <= n - 1; ++i) { // busquem el màxim de la columna
+	double max_col = abs(a[k][k]);
+	int max_pos = k;
+	for(int i = k + 1; i <= n - 1; ++i) { // busquem el màxim de la columna
 		double aik = abs(a[i][k]);
 		if (max_col < aik) {
 			max_col = aik;
 			max_pos = i;
 		}
+		if(max_col < tol) return 0;
 		// hem trobat el màx, i ara permutem la fila on es troba el màxim amb la fila k-èssima
-		swap(a[max_pos], a[k]);
 	}
-	return 1;
+	return max_pos;
+	//retornar -1 si max_col < tol
 
 }
 
 int lu(matriu& a, int n, fila perm, double tol) { //
-	for(int k = 0; k <= n - 1; ++k) {
+	for(int k = 0; k < n - 1; ++k) {
+		int max_pos = pivotatge_parcial(a, n, tol, k); // he borat lo del pivot
+			swap(a[max_pos], a[k]);
+			perm[k] = max_pos;
 		for(int i = k + 1; i <= n - 1; ++i) {
-			//Anem a fer pivotatge parcial
-			double pivot;
-			if (pivotatge_parcial( a, n, perm, tol, k, pivot) == 0) return 0;
-
-
 			double m = a[i][k]/a[k][k]; // m = m[i][k]
 			for(int j = k; j <= n - 1; ++j) {
 				a[i][j] = a[i][j] - m*a[k][j];
